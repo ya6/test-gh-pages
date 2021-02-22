0a398fd6315214fe4fbc4e4fdc100d94a9e9006c
YA <vector_cp@mail.ru>
2021-02-22 16:00:22 +0300

    Updates

---
+++ precache-manifest.7a0160cc975b56602de604e820ced369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e08a996882cc66b47cb1",
+    "url": "/test-gh-pages/static/css/main.20e49439.chunk.css"
+  },
+  {
+    "revision": "e08a996882cc66b47cb1",
+    "url": "/test-gh-pages/static/js/main.fab5068d.chunk.js"
+  },
+  {
+    "revision": "cb13c562ecdfc0f43177",
+    "url": "/test-gh-pages/static/js/runtime~main.b48b8b6b.js"
+  },
+  {
+    "revision": "886bbe336a570ed1e000",
+    "url": "/test-gh-pages/static/js/2.6a952a6f.chunk.js"
+  },
+  {
+    "revision": "6b20186f886982884432",
+    "url": "/test-gh-pages/static/js/3.e6e667ec.chunk.js"
+  },
+  {
+    "revision": "30a91970f235cc78fb9d602e414df0cb",
+    "url": "/test-gh-pages/index.html"
+  }
+];
